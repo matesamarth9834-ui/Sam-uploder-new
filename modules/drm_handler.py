a2428e9d1067a4cc216c6235d03666d120e750c1
matesamarth9834-ui <matesamarth9834@gmail.com>
2025-09-22 17:54:48 +0530

    Update drm_handler.py

--- modules/drm_handler.py
+++ modules/drm_handler.py
@@ -297,7 +297,7 @@ async def drm_handler(bot: Client, m: Message):
                 result = helper.get_mps_and_keys2(url)
                 if result is None:
                     await m.reply_text(f"❌ Token failed. Trying next one...")
-                    time.sleep(10)
+                    time.sleep(20)
                     result = helper.get_mps_and_keys2(url)                
                 mpd, keys = result
                 url = mpd
@@ -313,7 +313,7 @@ async def drm_handler(bot: Client, m: Message):
                 result = helper.get_mps_and_keys3(url)
                 if result is None:
                     await m.reply_text(f"❌ Token failed. Trying next one...")
-                    time.sleep(10)
+                    time.sleep(20)
                     result = helper.get_mps_and_keys3(url)
                 mpd = result    
                 mpd = helper.get_mps_and_keys3(url) 
@@ -327,7 +327,7 @@ async def drm_handler(bot: Client, m: Message):
                 result = helper.get_mps_and_keys2(url)
                 if result is None:
                     await m.reply_text(f"❌ Token failed. Trying next one...")
-                    time.sleep(10)
+                    time.sleep(20)
                     result = helper.get_mps_and_keys2(url)                
                 mpd, keys = result
                 url = mpd
